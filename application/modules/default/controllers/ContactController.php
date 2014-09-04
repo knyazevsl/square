475d6cd2b50a7137ceaf0b43ddc2747887c5f7b6
Stas <knyazevsl@gmail.com>
2014-09-05 00:18:24 +0400

    Finished chapter 3

---
+++ application/modules/default/controllers/ContactController.php
@@ -0,0 +1,37 @@
+<?php
+class ContactController extends Zend_Controller_Action
+{
+
+  public function init()
+  {
+    $this->view->doctype('XHTML1_STRICT');
+  }
+  
+  public function indexAction()
+  {
+    $form = new Square_Form_Contact();
+    $this->view->form = $form;             
+    if ($this->getRequest()->isPost()) {
+      if ($form->isValid($this->getRequest()->getPost())) {
+        $values = $form->getValues();
+        $mail = new Zend_Mail();
+        $mail->setBodyText($values['message']);
+        $mail->setFrom($values['email'], $values['name']);
+        $mail->addTo('knyazevsl@gmail.com');
+        $mail->setSubject('Contact form submission');
+        $mail->send();
+        $this->_helper->getHelper('FlashMessenger')->addMessage('Thank you. Your message was successfully sent.');
+        $this->_redirect('/contact/success');
+      }           
+    } 
+  }
+  
+  public function successAction()
+  {
+    if ($this->_helper->getHelper('FlashMessenger')->getMessages()) {
+      $this->view->messages = $this->_helper->getHelper('FlashMessenger')->getMessages();    
+    } else {
+      $this->_redirect('/');    
+    } 
+  }
+}
